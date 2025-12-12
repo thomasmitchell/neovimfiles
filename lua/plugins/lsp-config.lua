9b7a756591811eb76b092d111854a48105fcbbf5
Thomas Mitchell <klaue@live.com>
2025-12-12 16:16:58 -0500

    fix lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,12 +1,12 @@
 return {
   'neovim/nvim-lspconfig',
   init = function()
-    require'lspconfig'.gopls.setup{}
-    require'lspconfig'.rust_analyzer.setup{}
-    require'lspconfig'.terraformls.setup{}
-    require'lspconfig'.pyright.setup{}
-    require'lspconfig'.yamlls.setup{}
-    require'lspconfig'.lua_ls.setup{}
-    require'lspconfig'.helm_ls.setup{}
+    vim.lsp.enable('gopls')
+    vim.lsp.enable('rust_analyzer')
+    vim.lsp.enable('terraformls')
+    vim.lsp.enable('pyright')
+    vim.lsp.enable('yamlls')
+    vim.lsp.enable('lua_ls')
+    vim.lsp.enable('helm_ls')
   end
 }
